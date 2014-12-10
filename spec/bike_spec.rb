e16c735cffbb06b1d8c3f878accd341173e25d50
Sanda Golcea <sanda.golcea@gmail.com>
2014-12-10 12:05:47 +0000

    Initial commit: adds bike class & test

---
+++ spec/bike_spec.rb
@@ -0,0 +1,16 @@
+require 'bike'
+
+
+describe Bike do
+	
+	let(:the_bike) {Bike.new}
+
+	it 'should not be broken after we create it' do
+		expect(the_bike).not_to be_broken
+	end
+
+	it 'should be able to break' do 
+		the_bike.break!
+		expect(the_bike).to be_broken
+	end
+end
